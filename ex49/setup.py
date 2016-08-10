ebdf307f6d8ef95254ceb31656c2cd5a841c0fa4
ariawanzero <ariawan.zero@gmail.com>
2016-08-10 18:28:13 +0700

    Learn Python Hardway Study 49

---
+++ ex49/setup.py
@@ -0,0 +1,19 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+config = {
+    'description': 'My Project',
+    'author': 'Ariawan Pramadi',
+    'url': 'URL to get it at.',
+    'download_url': 'Where to download it.',
+    'author_email': 'ariawan.zero@gmail.com',
+    'version': '0.1',
+    'install_requires': ['nose'],
+    'packages': ['ex49'],
+    'scripts': [],
+    'name': 'ex49'
+}
+
+setup(**config)
